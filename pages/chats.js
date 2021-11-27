d944f542dcc7355d9f0d75842f341f24c1a3e9ba
zoebelete <hiwotbelete04@gmail.com>
2021-11-27 06:28:59 -0800

    all

---
+++ pages/chats.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useContext } from "react";
+
+import { Context } from "../context";
+
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFormSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
+
+export default function Chats() {
+  const { username, secret } = useContext(Context);
+  const [showChat, setShowChat] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof document !== undefined) {
+      setShowChat(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (username === "" || secret === "") {
+      router.push("/");
+    }
+  }, [username, secret]);
+
+  if (!showChat) return <div />;
+
+  return (
+    <div className="background">
+      <div className="shadow">
+        <ChatEngine
+          height="calc(100vh - 212px)"
+          projectID="b5799627-4a4a-4023-a29e-351ea3bbbcbd"
+          userName={username}
+          userSecret={secret}
+          renderNewMessageForm={() => <MessageFormSocial />}
+        />
+      </div>
+    </div>
+  );
+}
